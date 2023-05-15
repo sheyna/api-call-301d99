21c2b8e28c731b804134c61b88826c0667dd4378
sheyna <sheynawatkins@gmail.com>
2023-05-14 22:19:17 -0700

    start code.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>Data from an API</h1>
+        <form>
+          <button>Display Star Wars data</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default App;
