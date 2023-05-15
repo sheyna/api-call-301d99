297109c3567c972483a73c2a9881f05158e3dc78
sheyna <sheynawatkins@gmail.com>
2023-05-15 11:54:22 -0700

    got SW characters working added .env.sample.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import axios from 'axios'; // axios is an object that has several methods 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      starWarsData: []
     }
   }
 
+  // WHEN DEALING WITH AXIOS YOU NEED 3 things:
+  // - 1) async
+  // - 2) await
+  // - 3) .data
+  handleSWSubmit = async (event) => {
+    event.preventDefault();
+    // get the star Wars data from the API
+    // .get() makes a request to an API
+    // it take in a URL (as a string) as a parameter
+    let swChars = await axios.get('https://www.swapi.tech/api/people/?page=1');
+    // proof of life
+    // console.log(swChars.data.results);
+    // save it in state
+    this.setState({
+      starWarsData: swChars.data.results
+    });
+  }
+
+
+
   render() {
+    let swList = this.state.starWarsData.map((char, idx) => {
+      // console.log(char);
+      return <li key={idx}>{char.name}</li>;
+    });
 
     return (
       <>
         <h1>Data from an API</h1>
-        <form>
-          <button>Display Star Wars data</button>
+        <form onSubmit={this.handleSWSubmit}>
+          <button type="submit">Display Star Wars data</button>
         </form>
+        <ul>{swList}</ul>
       </>
     );
   }
